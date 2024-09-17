7902d22cf3f2544689d2e9ba429a8f5be4f2b3c9
Atividade Inicial - Cleiton <renanspeorin@outlook.com>
2024-09-16 22:22:10 -0300

    atividade 01

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numero;
+        int soma;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o numero da tabuada");
+        numero = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+
+            soma = numero * i;
+            System.out.println("Tabuada " + i + ": " + soma);
+        }
+    }
+}
